eb258731fb3cb06ff7d4bd3edf3d849c2c160f90
Shlok Singh <theshlok@gmail.com>
2023-06-06 14:46:14 +0530

    data validation modules completed

--- src/TextSummarizer/config/configuration.py
+++ src/TextSummarizer/config/configuration.py
@@ -1,6 +1,7 @@
 from TextSummarizer.constants import *
 from TextSummarizer.utils.common import read_yaml,create_directories
 from TextSummarizer.entity import DataIngestionConfig
+from TextSummarizer.entity import DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -10,7 +11,7 @@ class ConfigurationManager:
         
         self.config= read_yaml(config_filepath)
         self.params= read_yaml(params_filepath)
-
+        create_directories([self.config.artifacts_root])
 
     def get_data_ingestion_config(self)->DataIngestionConfig:
 
@@ -25,3 +26,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self)->DataValidationConfig:
+
+        config=self.config.data_validation
+        create_directories([config.root_dir])
+        
+        data_validation_config= DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES
+        )
+
+        return data_validation_config
+    
+
--- src/TextSummarizer/entity/__init__.py
+++ src/TextSummarizer/entity/__init__.py
@@ -9,3 +9,10 @@ class DataIngestionConfig: #This is a dataclass not a python class
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list    
